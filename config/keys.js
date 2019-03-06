aa4d1ef19dbfbcf53358ae4a3494650b97c8a05d
DhengreAni18 <dhengreani18@gmail.com>
2019-03-07 00:37:03 +0530

    POST request and user register ADDED

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb://Animesh:${encodeURIComponent(
-    "Animesh@123"
-  )}@mern-shard-00-00-jnjd7.mongodb.net:27017,mern-shard-00-01-jnjd7.mongodb.net:27017,mern-shard-00-02-jnjd7.mongodb.net:27017/test?ssl=true&replicaSet=MERN-shard-0&authSource=admin&retryWrites=true`
+  mongoURI:
+    "mongodb+srv://Animesh:7898204006@devconnect-kzqmr.mongodb.net/test?retryWrites=true"
 };
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello I m devConnect"));
 
