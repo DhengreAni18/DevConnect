8034176c33865b4be7256a90383bf06b7c691bf0
DhengreAni18 <dhengreani18@gmail.com>
2019-03-07 01:47:59 +0530

    Login validation Added

--- routes/api/users.js
+++ routes/api/users.js
@@ -40,4 +40,23 @@ router.post("/register", (req, res) => {
   });
 });
 
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "incorrect password" });
+      }
+    });
+  });
+});
+
 module.exports = router;
