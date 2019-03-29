45fb184201168bc95b0e01df8475613f99c6b527
Animesh Dhengre <dhengreani18@gmail.com>
2019-03-29 16:36:56 +0530

    Added Validations for Authentications

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,10 +7,17 @@ const keys = require("../../config/keys");
 const passport = require("passport");
 
 const User = require("../../models/User");
+const  validateRegisterInput = require('../../validation/register');
 
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 router.post("/register", (req, res) => {
+  const {errors, isValid } = validateRegisterInput(req.body);
+
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
