026cac5ae9c32d7e9d9ac3749164f00ef1de10a0
Animesh Dhengre <dhengreani18@gmail.com>
2019-04-01 04:36:49 +0530

    Adding Current User profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
 
 router.get("/test", (req, res) => res.json({ msg: "profile works" }));
 
