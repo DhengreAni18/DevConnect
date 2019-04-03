fcc3e100a1e268d5b05b06fd8be019361b55540f
Animesh Dhengre <dhengreani18@gmail.com>
2019-04-04 02:39:47 +0530

    Added Profile Protected route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,4 +10,18 @@ const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "profile works" }));
 
+router.get("/", passport.authenticate('jwt' , {session:false}), (req,res) => {
+Profile.findOne({ user : req.user.id})
+.then( profile => {
+    if(!profile) {
+    errors.noprofile = 'There is no profile for this user';
+    return res.status(404).json(errors);
+    }
+    res.json(profile);
+
+})
+.catch (err => res.status(404).json(err))
+});
+
+
 module.exports = router;
