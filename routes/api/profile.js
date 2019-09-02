2fa967a0e67b0daadaf0e22f86e1941d5ad784a3
DhengreAni18 <dhengreani18@gmail.com>
2019-09-03 00:47:56 +0530

    Added education and experience routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -40,7 +42,6 @@ router.get('/all' , (req,res) => {
     })
     .catch(err => res.status(404).json({profile:'There are no profiles'}));
 });
-
 
 
 
@@ -86,9 +87,7 @@ router.post(
         if(!isValid) {
             return res.status(400).json(errors);
         }
-
-
-
+ 
         const profileFields = {};
         profileFields.user = req.user.id;
         if(req.body.handle) profileFields.handle = req.body.handle;
@@ -132,4 +131,54 @@ router.post(
         });
     }
 );
+
+router.post('/experience', passport.authenticate('jwt', {session:false}), (req,res) => {
+    const { errors, isValid} = validateExperienceInput(req.body);
+
+        if(!isValid) {
+            return res.status(400).json(errors);
+        }
+
+    Profile.findOne({user:req.user.id})
+    .then(profile => {
+        const newExp = {
+            title:req.body.title,
+            company:req.body.company,
+            location:req.body.location,
+            from:req.body.from,
+            to:req.body.to,
+            current:req.body.current,
+            description:req.body.description
+
+        }
+
+        profile.experience.unshift(newExp);
+        profile.save().then(profile => res.json(profile));
+    })
+});
+
+router.post('/education', passport.authenticate('jwt', {session:false}), (req,res) => {
+    const { errors, isValid} = validateEducationInput(req.body);
+
+        if(!isValid) {
+            return res.status(400).json(errors);
+        }
+
+    Profile.findOne({user:req.user.id})
+    .then(profile => {
+        const newEdu = {
+            school:req.body.school,
+            degree:req.body.degree,
+            fieldofstudy:req.body.fieldofstudy,
+            from:req.body.from,
+            to:req.body.to,
+            current:req.body.current,
+            description:req.body.description
+
+        }
+
+        profile.education.unshift(newEdu);
+        profile.save().then(profile => res.json(profile));
+    })
+});
 module.exports = router;
