3d8eca1fff47fd4b9256a08bfbfa4aaf03b7e070
DhengreAni18 <dhengreani18@gmail.com>
2019-09-26 00:23:48 +0530

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>devnetwork</h1>
+      <header className="App-header"> 
+      <Navbar />
+      <h1></h1>
       </header>
     </div>
   );
